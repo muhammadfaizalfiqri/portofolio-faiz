e9c8b4fdafb6590f1aae0c4d2abf83fdb3f77340
Faiz Al Fiqri <12150113184@students.uin-suska.ac.id>
2026-07-24 19:08:14 +0700

    Fix responsive layout

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ navLinks.forEach(link => {
     });
 
 });
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+    const icon = menuToggle.querySelector("i");
+
+    if(navMenu.classList.contains("active")){
+
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+
+    }else{
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    }
+
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        navMenu.classList.remove("active");
+
+        const icon = menuToggle.querySelector("i");
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    });
+
+});
